12acb9876225273191bb26d350629743a29b39d1
Малофеев Андрей <andrew.malofeev@rcntec.com>
2025-08-26 07:18:19 +0300

    add: autoinstall/

---
+++ autoinstall/config.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+PI_VERSION="3.11.4"
+
+PI_ADMIN_USER="admin"
+PI_ADMIN_PASS="admin"
+PI_HOST="0.0.0.0"
+PI_PORT=5000
+PI_URL="http://privacyidea:5000/validate/check"
+PI_DB_FILE=/etc/privacyidea/users.sqlite
+FREERAD_SECRET="testing123"
